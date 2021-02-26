c79be9a19507a69df8ee6d62b78f55ca01b096da
Emanoel <eikymanoel09@gmail.com>
2021-02-26 13:48:16 -0300

    salve

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('Ola-mundo app is running!');
+    expect(await page.getTitleText()).toEqual('Olá Mundo!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'Ola-mundo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Ola-mundo');
+    expect(app.title).toEqual('Olà Mundo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Ola-mundo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Olá Mundo!');
   });
 });
